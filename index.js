98d6f7703d3086de02e5a79c789b4f13d0a7a6dd
Muntasir <alimuntasir2001@gmail.com>
2026-03-10 23:57:26 +0600

    Mongodb Connection+Testimonials dynamic   Done

--- index.js
+++ index.js
@@ -1,12 +1,79 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@simple-crud-server.a0arf8b.mongodb.net/?appName=simple-crud-server`;
+
+
+// MongoDB Client
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
 // Middleware
 app.use(cors());
 app.use(express.json());
+
+
+
+
+
+
+
+async function run() {
+  try {
+   
+    const db = client.db("Shopnest"); 
+    const testimonialCollection = db.collection("testimonials"); 
+
+    // ----------------------------------------------ALL Routes------------------------------------------------
+
+    // GET Route
+    app.get('/testimonials', async (req, res) => {
+      const cursor = testimonialCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+
+    // -----------------------------------------------ALL Routes------------------------------------------------
+
+  } finally {
+    
+  }
+}
+run().catch(console.dir);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 app.get('/', (req, res) => {
   res.send('ShopNest Server is Running...');
